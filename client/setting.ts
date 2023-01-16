4f5c1f8deb10fd2e34a36660fbff3691adba1092
osawa-koki <github@osawa.dev>
2023-01-16 21:22:50 +0900

    設定JS

---
+++ client/setting.ts
@@ -0,0 +1,12 @@
+
+const Env = require('./next.config.js');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const Setting = {
+  isProd,
+  basePath: Env.basePath,
+  apiPath: isProd ? '' : 'http://localhost:80',
+};
+
+export default Setting;
