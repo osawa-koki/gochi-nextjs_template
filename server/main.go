f6493049b19a4270c9fd10c5f35bf31507f0cac5
osawa-koki <github@osawa.dev>
2023-01-16 20:31:24 +0900

    基本的な動作確認まで。

--- server/main.go
+++ server/main.go
@@ -1,44 +1,45 @@
 package main
 
 import (
-    "net/http"
-    "os"
-    "path/filepath"
+	"net/http"
+	"os"
+	"path/filepath"
 
-    "github.com/go-chi/chi"
+	"github.com/go-chi/chi"
 )
 
 func apiIndex(w http.ResponseWriter, r *http.Request) {
-    // handle GET /api request
+	w.Write([]byte("Hello I am a GET man."))
 }
 
 func apiCreate(w http.ResponseWriter, r *http.Request) {
-    // handle POST /api/create request
+	w.Write([]byte("Hello I am a POST man."))
 }
 
 func apiUpdate(w http.ResponseWriter, r *http.Request) {
-    // handle PUT /api/update request
+	w.Write([]byte("Hello I am a PUT man."))
 }
 
 func apiDelete(w http.ResponseWriter, r *http.Request) {
-    // handle DELETE /api/delete request
+	w.Write([]byte("Hello I am a DELETE man."))
 }
 
 func main() {
-    r := chi.NewRouter()
+	r := chi.NewRouter()
 
-    // API routes
-    r.Route("/api", func(r chi.Router) {
-        r.Get("/", apiIndex)
-        r.Post("/create", apiCreate)
-        r.Put("/update", apiUpdate)
-        r.Delete("/delete", apiDelete)
-    })
+	// API routes
+	r.Route("/api", func(r chi.Router) {
+		r.Get("/", apiIndex)
+		r.Get("/get", apiIndex)
+		r.Post("/create", apiCreate)
+		r.Put("/update", apiUpdate)
+		r.Delete("/delete", apiDelete)
+	})
 
-    // Serve static files
-    workDir, _ := os.Getwd()
-    filesDir := http.Dir(filepath.Join(workDir, "web"))
-    r.Handle("/*", http.StripPrefix("/", http.FileServer(filesDir)))
+	// Serve static files
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "web"))
+	r.Handle("/*", http.StripPrefix("/", http.FileServer(filesDir)))
 
-    http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", r)
 }
