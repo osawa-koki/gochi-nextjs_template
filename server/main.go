df24bcc00fa34247c9a00d2d339a4c4602ecc5d3
osawa-koki <github@osawa.dev>
2023-01-16 20:28:45 +0900

    基本的な動作確認は完了。

--- server/main.go
+++ server/main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-	"net/http"
-	"os"
-	"path/filepath"
+    "net/http"
+    "os"
+    "path/filepath"
 
-	"github.com/go-chi/chi"
+    "github.com/go-chi/chi"
 )
 
 func apiIndex(w http.ResponseWriter, r *http.Request) {
-	// handle GET /api request
+    // handle GET /api request
 }
 
 func apiCreate(w http.ResponseWriter, r *http.Request) {
-	// handle POST /api/create request
+    // handle POST /api/create request
 }
 
 func apiUpdate(w http.ResponseWriter, r *http.Request) {
-	// handle PUT /api/update request
+    // handle PUT /api/update request
 }
 
 func apiDelete(w http.ResponseWriter, r *http.Request) {
-	// handle DELETE /api/delete request
+    // handle DELETE /api/delete request
 }
 
 func main() {
-	r := chi.NewRouter()
+    r := chi.NewRouter()
 
-	// API routes
-	r.Route("/api", func(r chi.Router) {
-		r.Get("/", apiIndex)
-		r.Post("/create", apiCreate)
-		r.Put("/update", apiUpdate)
-		r.Delete("/delete", apiDelete)
-	})
+    // API routes
+    r.Route("/api", func(r chi.Router) {
+        r.Get("/", apiIndex)
+        r.Post("/create", apiCreate)
+        r.Put("/update", apiUpdate)
+        r.Delete("/delete", apiDelete)
+    })
 
-	// Serve static files
-	workDir, _ := os.Getwd()
-	filesDir := filepath.Join(workDir, "web")
-	r.FileServer("/", http.Dir(filesDir))
+    // Serve static files
+    workDir, _ := os.Getwd()
+    filesDir := http.Dir(filepath.Join(workDir, "web"))
+    r.Handle("/*", http.StripPrefix("/", http.FileServer(filesDir)))
 
-	http.ListenAndServe(":3000", r)
+    http.ListenAndServe(":3000", r)
 }
