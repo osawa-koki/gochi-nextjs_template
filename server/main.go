4d0a4ae04d507c5b41ebab03c38b41652b245445
osawa-koki <github@osawa.dev>
2023-01-17 21:30:39 +0900

    サーバでAPIを返却するように。

--- server/main.go
+++ server/main.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/go-chi/chi"
 )
+
+type Lang struct {
+	Name string `json:"name"`
+	Birthday time.Time `json:"birthday"`
+	On_trend bool `json:"on_trend"`
+}
 
 func apiIndex(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"text": "I am a GET man."})
@@ -25,6 +32,46 @@ func apiDelete(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"text": "I am a DELETE man."})
 }
 
+func apiLang(w http.ResponseWriter, r *http.Request) {
+	var langs []Lang = []Lang{
+		{Name: "Go", Birthday: time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Rust", Birthday: time.Date(2010, time.May, 15, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "C", Birthday: time.Date(1972, time.June, 1, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "C++", Birthday: time.Date(1983, time.December, 1, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "Java", Birthday: time.Date(1995, time.May, 23, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "Python", Birthday: time.Date(1991, time.February, 20, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Ruby", Birthday: time.Date(1995, time.December, 21, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "JavaScript", Birthday: time.Date(1995, time.December, 4, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "PHP", Birthday: time.Date(1995, time.June, 8, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "C#", Birthday: time.Date(2000, time.November, 13, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "Swift", Birthday: time.Date(2014, time.June, 2, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Kotlin", Birthday: time.Date(2011, time.February, 15, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Dart", Birthday: time.Date(2011, time.June, 10, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "R", Birthday: time.Date(1993, time.October, 1, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Scala", Birthday: time.Date(2003, time.February, 20, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Elixir", Birthday: time.Date(2011, time.June, 23, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Clojure", Birthday: time.Date(2007, time.May, 1, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Haskell", Birthday: time.Date(1990, time.July, 1, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Erlang", Birthday: time.Date(1986, time.January, 1, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "F#", Birthday: time.Date(2005, time.July, 1, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Julia", Birthday: time.Date(2012, time.February, 20, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Lua", Birthday: time.Date(1993, time.March, 1, 23, 0, 0, 0, time.UTC), On_trend: true},
+		{Name: "Perl", Birthday: time.Date(1987, time.December, 18, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "Groovy", Birthday: time.Date(2003, time.May, 1, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "Objective-C", Birthday: time.Date(1984, time.March, 1, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "Visual Basic", Birthday: time.Date(1991, time.August, 1, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "Delphi", Birthday: time.Date(1995, time.May, 23, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "Pascal", Birthday: time.Date(1970, time.January, 1, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "COBOL", Birthday: time.Date(1959, time.August, 1, 23, 0, 0, 0, time.UTC), On_trend: false},
+		{Name: "Fortran", Birthday: time.Date(1957, time.June, 1, 23, 0, 0, 0, time.UTC), On_trend: false},
+	}
+	jsonData, err := json.Marshal(langs); if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
+}
+
 func main() {
 	r := chi.NewRouter()
 
@@ -35,6 +82,7 @@ func main() {
 		r.Post("/", apiCreate)
 		r.Put("/", apiUpdate)
 		r.Delete("/", apiDelete)
+		r.Get("/lang", apiLang)
 	})
 
 	// Serve static files
